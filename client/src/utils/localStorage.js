8aceaa4325531e5a0ddff433ec90c96c7c66cfac
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-25 19:44:24 +1000

    added localStorage.js to save and delete a mood

---
+++ client/src/utils/localStorage.js
@@ -0,0 +1,26 @@
+export const getSavedMoodIds = () => {
+	const savedMoodIds = localStorage.getItem("saved_moods") ? JSON.parse(localStorage.getItem("saved_moods")) : [];
+
+	return savedMoodIds;
+};
+
+export const saveMoodIds = (moodIdArr) => {
+	if (moodIdArr.length) {
+		localStorage.setItem("saved_moods", JSON.stringify(moodIdArr));
+	} else {
+		localStorage.removeItem("saved_moods");
+	}
+};
+
+export const removeMoodId = (moodId) => {
+	const savedMoodIds = localStorage.getItem("saved_moods") ? JSON.parse(localStorage.getItem("saved_moods")) : null;
+
+	if (!savedMoodIds) {
+		return false;
+	}
+
+	const updatedSavedMoodIds = savedMoodIds?.filter((savedMoodId) => savedMoodId !== moodId);
+	localStorage.setItem("saved_moods", JSON.stringify(updatedSavedMoodIds));
+
+	return true;
+};
