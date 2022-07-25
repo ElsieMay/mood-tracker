d260688530c25b70f17158b218da6896175ba574
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-25 19:39:25 +1000

    added API.js file

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from "./NavbarElements";
 
 export const Navbar = () => {
 	return (
 		<>
 			<Nav>
 				<NavbarContainer>
-					<NavLogo>link</NavLogo>
+					<NavLogo to="/">mood</NavLogo>
+					<MobileIcon>
+						<FaBars />
+					</MobileIcon>
+					<NavMenu>
+						<NavItem>
+							<NavLinks to="mood">My Mood</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to="me">My Month</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to="services">Services</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to="addUser">Sign Up</NavLinks>
+						</NavItem>
+						<NavBtn>
+							<NavBtnLink to="/login">Login</NavBtnLink>
+						</NavBtn>
+					</NavMenu>
 				</NavbarContainer>
 			</Nav>
 		</>
