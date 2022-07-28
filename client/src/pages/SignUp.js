3986fc120b00edc084a26e910b47403322427772
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-28 20:05:17 +1000

    fixed bugs in validation

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
-import { Button } from "../components/Button";
+// import { Button } from "../components/Button";
 import Navbar from "../components/Navbar/index";
 import { Sidebar } from "../components/Sidebar";
-import styled from "styled-components";
-import { Form, Alert } from "react-bootstrap";
+// import styled from "styled-components";
+import { Form, Alert, Button } from "react-bootstrap";
 import { FormContainer, FormWrapper, FormContent } from "../components/SignUp/SignUpElements";
 import { Icon } from "../components/Sidebar/SidebarElements";
 import MainLogo from "../components/assets/mood-logo.png";
 import { NavLogo } from "../components/Navbar/NavbarElements";
 import HeroSignUp from "../components/SignUp/index";
-import "../components/assets/laya-clode-Zf4yTni1CWg-unsplash.jpg";
+// import "../components/assets/laya-clode-Zf4yTni1CWg-unsplash.jpg";
 
 const SignUp = () => {
 	const [userData, setUserData] = useState({ username: "", email: "", password: "" });
@@ -31,46 +31,46 @@ const SignUp = () => {
 		setIsOpen(!isOpen);
 	};
 
-	const Wrapper = styled.div`
-		display: flex;
-		flex-direction: column;
-	`;
+	// const Wrapper = styled.div`
+	// 	display: flex;
+	// 	flex-direction: column;
+	// `;
 
-	const StyledFormLabel = styled(Form.Label)`
-		color: white;
-		font-size: 20px;
-	`;
+	// const StyledFormLabel = styled(Form.Label)`
+	// 	color: white;
+	// 	font-size: 20px;
+	// `;
 
-	const StyledFormControl = styled(Form.Control)`
-		color: black;
-	`;
+	// const StyledFormControl = styled(Form.Control)`
+	// 	color: black;
+	// `;
 
-	const StyledFormControlFeedback = styled(Form.Control.Feedback)`
-		color: white;
-	`;
+	// const StyledFormControlFeedback = styled(Form.Control.Feedback)`
+	// 	color: white;
+	// `;
 
-	const StyledFormGroup = styled(Form.Group)`
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-between;
-		padding: 20px;
-	`;
+	// const StyledFormGroup = styled(Form.Group)`
+	// 	display: flex;
+	// 	flex-direction: column;
+	// 	align-items: center;
+	// 	justify-content: space-between;
+	// 	padding: 20px;
+	// `;
 
-	const StyledForm = styled(Form)`
-		margin-bottom: 40px;
-	`;
+	// const StyledForm = styled(Form)`
+	// 	margin-bottom: 40px;
+	// `;
 
-	const StyledButton = styled(Button)`
-		width: 200px;
-	`;
+	// const StyledButton = styled(Button)`
+	// 	width: 200px;
+	// `;
 
 	const handleForm = async (event) => {
 		console.log("here");
 		event.preventDefault();
 
 		const form = event.currentTarget;
-		if (form.validation() === false) {
+		if (form.checkValidity() === false) {
 			event.preventDefault();
 			event.stopPropagation();
 		}
@@ -91,49 +91,43 @@ const SignUp = () => {
 			password: "",
 		});
 	};
-	img: require("../components/assets/laya-clode-Zf4yTni1CWg-unsplash.jpg");
+	// img: require("../components/assets/laya-clode-Zf4yTni1CWg-unsplash.jpg");
 	return (
 		<>
 			<div>
 				<Sidebar isOpen={isOpen} toggle={toggle} />
-				<Wrapper>
+				<div>
 					<Navbar toggle={toggle} />
-				</Wrapper>
+				</div>
 			</div>
 			<HeroSignUp />
 			<div>
-				<FormContainer>
-					<FormWrapper>
-						<FormContent>
-							<NavLogo to="/mood-tracker">
-								<img src={MainLogo} alt="logo" width="110px" />
-							</NavLogo>
-							<StyledForm noValidate validated={validated} onSubmit={handleForm}>
-								<Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
-									Please try again
-								</Alert>
-								<StyledFormGroup>
-									<StyledFormLabel htmlFor="username">Username</StyledFormLabel>
-									<StyledFormControl type="text" placeholder="Your username" name="username" onChange={handleInputChange} value={userData.username} required />
-									<StyledFormControlFeedback type="invalid">Please enter a username</StyledFormControlFeedback>
-								</StyledFormGroup>
-								<StyledFormGroup>
-									<StyledFormLabel htmlFor="email">Email</StyledFormLabel>
-									<StyledFormControl type="email" placeholder="Your email address" name="email" onChange={handleInputChange} value={userData.email} required />
-									<StyledFormControlFeedback type="invalid">Please enter an email</StyledFormControlFeedback>
-								</StyledFormGroup>
-								<StyledFormGroup>
-									<StyledFormLabel htmlFor="password">Password</StyledFormLabel>
-									<StyledFormControl type="password" placeholder="Your password" name="password" onChange={handleInputChange} value={userData.password} required />
-									<StyledFormControlFeedback type="invalid">Please enter a password</StyledFormControlFeedback>
-								</StyledFormGroup>
-								<StyledButton disabled={!(userData.username && userData.email && userData.password)} type="submit" variant="success">
-									Submit
-								</StyledButton>
-							</StyledForm>
-						</FormContent>
-					</FormWrapper>
-				</FormContainer>
+				<NavLogo to="/mood-tracker">
+					<img src={MainLogo} alt="logo" width="110px" />
+				</NavLogo>
+				<Form noValidate validated={validated} onSubmit={handleForm}>
+					<Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
+						Please try again
+					</Alert>
+					<Form.Group>
+						<Form.Label htmlFor="username">Username</Form.Label>
+						<Form.Control type="text" placeholder="Your username" name="username" onChange={handleInputChange} value={userData.username} required />
+						<Form.Control.Feedback type="invalid">Please enter a username</Form.Control.Feedback>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label htmlFor="email">Email</Form.Label>
+						<Form.Control type="email" placeholder="Your email address" name="email" onChange={handleInputChange} value={userData.email} required />
+						<Form.Control.Feedback type="invalid">Please enter an email</Form.Control.Feedback>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label htmlFor="password">Password</Form.Label>
+						<Form.Control type="password" placeholder="Your password" name="password" onChange={handleInputChange} value={userData.password} required />
+						<Form.Control.Feedback type="invalid">Please enter a password</Form.Control.Feedback>
+					</Form.Group>
+					<Button disabled={!(userData.username && userData.email && userData.password)} type="submit" variant="success">
+						Submit
+					</Button>
+				</Form>
 			</div>
 		</>
 	);
