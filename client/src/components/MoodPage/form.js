0da8491c29ec890ffa031a1294b7157cb944353d
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-29 22:31:43 +1000

    fixed bug with button, passed data to console from form selection

--- client/src/components/MoodPage/form.js
+++ client/src/components/MoodPage/form.js
@@ -1,34 +1,27 @@
-import React, { useState } from "react";
-import { Button } from "../Button";
-import { ArrowForward, ArrowRight } from "../HomePage/HomePageElements";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-const Form = () => {
-	const [hover, setHover] = useState(false);
+export default function MoodForm() {
+	const { register, handleSubmit } = useForm();
+	const onSubmit = (data) => console.log(data);
 
-	const onHover = () => {
-		setHover(!hover);
-	};
 	return (
 		<div className="form max-w-sm mx-auto w-96">
 			<h1 className="font-bold pb-4 text-xl">Question 1</h1>
-			<form id="form">
+			<form id="form" onSubmit={handleSubmit(onSubmit)}>
 				<div className="grid gap-4">
 					<h4>How often have you been bothered by feeling down, depressed or hopeless?</h4>
-					<select className="form-input">
+					<select className="form-input mt-1 block w-full py-2.5 px-3 border border-gray-200 bg-white rounded-md focus:outline-none focus:ring-indigo-500 sm:text-sm" {...register("selection")}>
 						<option value="Never">Never</option>
 						<option value="Sometimes">Sometimes</option>
 						<option value="Frequently">Frequently</option>
 						<option value="Always">Always</option>
 					</select>
 					<div className="submit-btn">
-						<Button onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
-							Submit{hover ? <ArrowForward /> : <ArrowRight />}
-						</Button>
+						<button className="border py-2 text-white bg-green-500 w-full">Submit</button>
 					</div>
 				</div>
 			</form>
 		</div>
 	);
-};
-
-export default Form;
+}
--- client/src/components/MoodPage/index.js
+++ client/src/components/MoodPage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Graph from "./Graph";
-import Form from "./Form";
+import MoodForm from "./Form";
 
 const MoodPage = () => {
 	return (
@@ -10,7 +10,7 @@ const MoodPage = () => {
 					<h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Debrief on your mood today</h1>
 					<div className="grid md:grid-cols-2 gap-4">
 						<Graph></Graph>
-						<Form></Form>
+						<MoodForm></MoodForm>
 					</div>
 				</div>
 			</div>
