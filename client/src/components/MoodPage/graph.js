5aa093dfbdb01a892f2eaa0bb96687359cd23019
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-29 19:46:48 +1000

    added styling to graph

--- client/src/components/MoodPage/graph.js
+++ client/src/components/MoodPage/graph.js
@@ -10,10 +10,13 @@ const Graph = () => {
 		datasets: [
 			{
 				label: "# of Votes",
-				data: [12, 19, 3, 5, 2, 3],
+				data: [12, 35, 5, 5, 6, 8],
 				backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
 				borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+				hoverOffset: 4,
+				borderRadius: 10,
 				borderWidth: 1,
+				spacing: 10,
 			},
 		],
 	};
