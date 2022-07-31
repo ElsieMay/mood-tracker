9a1c43cea6819da551a108efc873153a111451a0
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-31 20:07:01 +1000

    changed name

--- client/src/components/MoodPage/index.js
+++ client/src/components/MoodPage/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Graph from "./graph";
-import Form from "./form";
+import Graph from "./Graph";
+import Form from "./Form";
 
 const MoodPage = () => {
 	return (
 		<div>
 			<div className="App">
 				<div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
-					<h1 className="text-2xl py-8 mb-10 bg-slate-800 text-white rounded">Debrief on your mood today</h1>
+					<h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Debrief on your mood today</h1>
 					<div className="grid md:grid-cols-2 gap-4">
 						<Graph></Graph>
 						<Form></Form>
