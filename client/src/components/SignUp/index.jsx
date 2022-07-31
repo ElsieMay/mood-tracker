fd8cdbd7e4e77011f169527cd983268a42206263
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-31 17:14:58 +1000

    added responsiveness to sign up page

--- client/src/components/SignUp/index.jsx
+++ client/src/components/SignUp/index.jsx
@@ -59,30 +59,28 @@ const Signup = () => {
 					<Navbar toggle={toggle} />
 				</div>
 			</div>
-			<div className={styles.form_wrapper}>
-				<div className={styles.signup_container}>
-					<img src={BackgroundImage} alt="image" height="2500" className={styles.image} />
-					<div className={styles.signup_form_container}>
-						<div className={styles.left}>
-							<h1>Welcome Back</h1>
-							<Link to="mood-tracker/login">
-								<button type="button" className={styles.white_btn}>
-									Login
-								</button>
-							</Link>
-						</div>
-						<div className={styles.right}>
-							<form className={styles.form_container} onSubmit={handleSubmit}>
-								<h1>Create Account</h1>
-								<input type="text" placeholder="Username" name="username" onChange={handleChange} value={data.username} required className={styles.input} />
-								<input type="email" placeholder="Email" name="email" onChange={handleChange} value={data.email} required className={styles.input} />
-								<input type="password" placeholder="Password" name="password" onChange={handleChange} value={data.password} required className={styles.input} />
-								{error && <div className={styles.error_msg}>{error}</div>}
-								<button type="submit" className={styles.green_btn}>
-									Sign Up
-								</button>
-							</form>
-						</div>
+			<div className={styles.signup_container}>
+				<img src={BackgroundImage} alt="image" height="2500" className={styles.image} />
+				<div className={styles.signup_form_container}>
+					<div className={styles.left}>
+						<h1>Welcome Back</h1>
+						<Link to="mood-tracker/login">
+							<button type="button" className={styles.white_btn}>
+								Login
+							</button>
+						</Link>
+					</div>
+					<div className={styles.right}>
+						<form className={styles.form_container} onSubmit={handleSubmit}>
+							<h1 className={styles.title}>Create Account</h1>
+							<input type="text" placeholder="Username" name="username" onChange={handleChange} value={data.username} required className={styles.input} />
+							<input type="email" placeholder="Email" name="email" onChange={handleChange} value={data.email} required className={styles.input} />
+							<input type="password" placeholder="Password" name="password" onChange={handleChange} value={data.password} required className={styles.input} />
+							{error && <div className={styles.error_msg}>{error}</div>}
+							<button type="submit" className={styles.green_btn}>
+								Sign Up
+							</button>
+						</form>
 					</div>
 				</div>
 			</div>
