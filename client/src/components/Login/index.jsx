e09b24a451e3362d745b0a841deceb08539e577f
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-31 17:50:04 +1000

    added responsiveness to login page

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -61,7 +61,7 @@ const Signup = () => {
 					<Navbar toggle={toggle} />
 				</div>
 			</div>
-			<div className={styles.login_container}>
+			{/* <div className={styles.login_container}>
 				<img src={BackgroundImage} alt="image" height="2500" className={styles.image} />
 				<div className={styles.login_form_container}>
 					<div className={styles.left}>
@@ -80,6 +80,30 @@ const Signup = () => {
 						<Link to="mood-tracker/signup">
 							<button type="button" className={styles.white_btn}>
 								Sign Up
+							</button>
+						</Link>
+					</div>
+				</div>
+			</div> */}
+			<div className={styles.login_container}>
+				<img src={BackgroundImage} alt="image" height="2500" className={styles.image} />
+				<div className={styles.login_form_container}>
+					<div className={styles.left}>
+						<form className={styles.form_container} noValidate validated={validated} onSubmit={handleFormSubmit}>
+							<h1 className={styles.title}>Login to Your Account</h1>
+							<input type="email" placeholder="Email" name="email" onChange={handleInputChange} value={userFormData.email} required className={styles.input} />
+							<input type="password" placeholder="Password" name="password" onChange={handleInputChange} value={userFormData.password} required className={styles.input} />
+							{error && <div className={styles.error_msg}>{error}</div>}
+							<button disabled={!(userFormData.email && userFormData.password)} type="submit" className={styles.green_btn}>
+								Sign In
+							</button>
+						</form>
+					</div>
+					<div className={styles.right}>
+						<h1>New Here ?</h1>
+						<Link to="/signup">
+							<button type="button" className={styles.white_btn}>
+								Sing Up
 							</button>
 						</Link>
 					</div>
