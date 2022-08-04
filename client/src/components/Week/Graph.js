1567d6a9e5c8ff4c90afd5f7da5ffe75343e13b6
ElsieMay <elsiemaylawrie1@gmail.com>
2022-08-04 12:14:36 +1000

    added data for weekly graph

--- client/src/components/Week/Graph.js
+++ client/src/components/Week/Graph.js
@@ -12,26 +12,25 @@ const WeekGraph = ({ savedMoods }) => {
 	const anxiousMoods = savedMoods.filter((x) => x.type === "anxious");
 	const lowMoods = savedMoods.filter((x) => x.type === "low");
 
-	const anxiousTotal = anxiousMoods.map((x) => x.value).reduce((a, b) => a + b, 0);
-	const lowTotal = lowMoods.map((x) => x.value).reduce((a, b) => a + b, 0);
+	const anxiousTotal = anxiousMoods.map((x) => x.value);
+	const lowTotal = lowMoods.map((x) => x.value);
 
-	const dateMap = anxiousMoods.map((x) => x.date);
-	// var date = dateMap;
+	const Date = savedMoods.map((x) => x.date);
 
-	console.log("DATE MAPPPP", dateMap);
+	// console.log("DATE MAPPPP", dateMap);
 
 	const data = {
-		labels: [dateMap[0], dateMap[1], dateMap[2]],
+		labels: [Date[0], Date[1], Date[2], Date[3], Date[4], Date[5], Date[6], Date[7], Date[8], Date[9], Date[10], Date[11], Date[12], Date[13], Date[14], Date[15], Date[16], Date[17]],
 		datasets: [
 			{
 				label: "Low Mood",
-				data: [lowTotal],
+				data: [lowTotal[0], lowTotal[1], lowTotal[2], lowTotal[3], lowTotal[4], lowTotal[5], lowTotal[6], lowTotal[7], lowTotal[8], lowTotal[9], lowTotal[10], lowTotal[11], lowTotal[12], lowTotal[13], lowTotal[14], lowTotal[15], lowTotal[16], lowTotal[17], lowTotal[18], lowTotal[19]],
 				fill: true,
 				borderColor: "#e1b37f",
 			},
 			{
 				label: "Anxiousness",
-				data: [anxiousTotal],
+				data: [anxiousTotal[0], anxiousTotal[1], anxiousTotal[2], anxiousTotal[3], anxiousTotal[4], anxiousTotal[5], anxiousTotal[6], anxiousTotal[7], anxiousTotal[8], anxiousTotal[9], anxiousTotal[10], anxiousTotal[11], anxiousTotal[12], anxiousTotal[13], anxiousTotal[14], anxiousTotal[15], anxiousTotal[16], anxiousTotal[17], anxiousTotal[18], anxiousTotal[19]],
 				fill: false,
 				borderColor: "rgba(177, 185, 91, 1)",
 			},
